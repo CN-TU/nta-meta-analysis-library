13158135895bd8699a0ab544d5fa76ab70b52fa2
Daniel Ferreira <dcferreira@users.noreply.github.com>
2018-02-28 14:02:28 +0100

    added script to list_fields

---
+++ list_field.py
@@ -0,0 +1,31 @@
+import argparse
+from ntarcpy import database as db
+
+
+description = """
+Gets existing values in the NTARC database for a specific field.
+"""
+
+
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument('database', type=str, help="Path to the database directory.")
+parser.add_argument('field', type=str, help="The field to inspect. Example: reference.authors.author")
+parser.add_argument('--per-paper', action='store_true',
+                    help="With this option, each duplicated values in the same paper are counted only once.")
+parser.add_argument('--papers', action='store_true',
+                    help="With this option, get the papers that use each value.")
+parser.add_argument('-F', '--sep', type=str, default=';',
+                    help="Separator to use in the output.")
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    if args.papers:
+        key = lambda x: len(x[1])
+        f = db.get_papers_per_field
+    else:
+        key = lambda x: x[1]
+        f = db.count_field
+
+    for k, v in sorted(f(args.database, args.field).items(), key=key, reverse=True):
+        print(k, v, sep=args.sep)
