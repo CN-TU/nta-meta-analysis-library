70b336ae9395af33cf70fe620a3e1c938d69ceff
Daniel Ferreira <dcferreira@users.noreply.github.com>
2018-08-20 11:23:37 +0200

    changed how to fetch version

--- configure.sh
+++ configure.sh
@@ -5,8 +5,9 @@ rm -r $DIR/ntarcpy/_version/*
 git clone https://github.com/CN-TU/nta-meta-analysis-specification /tmp/ntarc-spec || exit 1
 cd /tmp/ntarc-spec
 git worktree prune
-for VERSION in $( git branch --list "r*" | tr -d " *" ); do  # lists branches with name "r*", and then remove the '*', if it exists
+for VERSION in $( git branch -a --list | egrep -o "r[0-9].*$" ); do  # lists branches with name "r*", and then remove the '*', if it exists
+    echo $VERSION
     VERSION_PATH=${VERSION/r/v}
     path=$DIR/ntarcpy/_version/${VERSION_PATH/\./_}
-    git worktree add --force $path $VERSION
+    git worktree add --force $path remotes/origin/$VERSION
 done
